667b55f62cb1c859612135ebdf7633ec15fe85e6
Andrew Schaffer <andrew.schaffer@uconn.edu>
2016-03-10 15:45:23 -0500

    login indicator

--- www/js/services/drive.js
+++ www/js/services/drive.js
@@ -125,7 +125,7 @@ angular.module('TractNotes')
                     redirect_uri = options.redirect_uri;
                   }
                 }
-                var url = 'https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=token';
+                var url = 'https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=id_token token';
                 var browserRef = window.open(url, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
                 browserRef.addEventListener("loadstart", function (event) {
                   if ((event.url).indexOf(redirect_uri) === 0) {
